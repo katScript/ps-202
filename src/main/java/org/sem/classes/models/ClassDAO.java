48e77be9e7beb3b3bc66332c3e08bd716e58ac91
Hoàng Long <long.dt.2019@aptechlearning.edu.vn>
2022-12-07 20:18:47 +0700

    ClassDao

--- src/main/java/org/sem/classes/models/ClassDAO.java
+++ src/main/java/org/sem/classes/models/ClassDAO.java
@@ -99,22 +99,53 @@ public class ClassDAO implements DAOInterface<Class> {
     public Class save(Class aClass) {
         try {
             // 1.get connection
+            Connection con = this.connector
+                    .startConnection().getConnection();
 
             // 2.prepare query
                 // 2.1 check is new or not => aClass.getId() != null or not
                 // if new => use insert into query else update query
+                 String sql;
+            PreparedStatement ps;
+            
+            if (aClass.getId() != null) {
+                sql = String.format("UPDATE `%s` SET  id = ?, Class name = ?", getTableName());
+                ps = con.prepareStatement(sql);
+                
+                ps.setString(1, aClass.Long());
+                ps.setString(2, aClass.getClassName());
+                
+            } else {
+                sql = String.format("INSERT INTO `%s` (class_Name, id) VALUES (?,?,?)", getTableName());
+                ps = con.prepareStatement(sql);
+                
+                ps.setString(1, aClass.getClassName());
+                ps.setString(2, aClass.getLong());
+                
+            }
 
             // 3.execute query
+            Boolean result = ps.execute();
 
             // 4.process query return data
+            if (result) {
+                ResultSet rs = ps.getGeneratedKeys();
+                aClass.setId(rs.getLong(1));
+                
+                rs.close();
+            }
 
             // 5.close transaction
+            ps.close();
 
             // 6.return result
         } catch (Exception e) {
             // 7.handle errors
+            throw new RuntimeException(e);
+
         } finally {
             // 8.close database connection
+            this.connector.closeConnection();
         }
 
         return null;
@@ -124,23 +155,33 @@ public class ClassDAO implements DAOInterface<Class> {
     public Boolean delete(Class aClass) {
         try {
             // 1.get connection
+            Connection con = this.connector
+                   .startConnection().getConnection();
 
             // 2.prepare query
+            String sql = String.format("DELETE FROM `%s` WHERE `id` = ?", getTableName());
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setLong(1, aClass.getId());
+           
 
             // 3.execute query
+            Boolean result = ps.execute();
 
             // 4.process query return data true or false => delete success or not
+            ps.close();
 
             // 5.close transaction
+            return result;
 
             // 6.return result
         } catch (Exception e) {
             // 7.handle errors
+             throw new RuntimeException(e);
         } finally {
             // 8.close database connection
+            this.connector.closeConnection();
         }
 
-        return null;
     }
 
     public Connector getConnector() {
