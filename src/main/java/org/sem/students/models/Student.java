1618b3b313653bab0c2528af1ae95958599a9080
Hung <dinhhung.ntmk@gmail.com>
2022-12-07 20:27:35 +0700

    Student

---
+++ src/main/java/org/sem/students/models/Student.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.sem.students.models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Student {
+    private Long id;
+    private String roll_number;
+    private String fullname;
+    private String email;
+    private String phone;
+    private Boolean gender;
+    private Date dob;
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(Long id, String roll_number, String fullname, String email, String phone, Boolean gender, Date dob, String address) {
+        this.id = id;
+        this.roll_number = roll_number;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.dob = dob;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoll_number() {
+        return roll_number;
+    }
+
+    public void setRoll_number(String roll_number) {
+        this.roll_number = roll_number;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
